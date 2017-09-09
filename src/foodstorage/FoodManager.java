b4768615d1789508ba9fde8de4c43da875672abf
Darcy Clark <dgrclark@edu.uwaterloo.ca>
2017-09-09 19:35:56 -0400

    Added adding storage units.

--- src/foodstorage/FoodManager.java
+++ src/foodstorage/FoodManager.java
@@ -3,20 +3,42 @@ package foodstorage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class FoodManager {
 
+    private static ArrayList<StorageUnit> allStorageUnits = new ArrayList<>();
+
+    private static final String COM_ADD_SU = "adds";
+
     public static void main(String args[]) {
         BufferedReader userCommandBR =  new BufferedReader(new InputStreamReader(System.in));
-        String userCommand = new String();
+        String[] userCommand = new String[1];
+        userCommand[0] = "-";
 
-        while(!userCommand.equals("-e")) {
+        while(!userCommand[0].equals("e")) {
             System.out.print("Enter a command: ");
             try {
-                userCommand = userCommandBR.readLine();
-                System.out.println("You entered: " + userCommand + "\nThank you for your input.\n");
+                userCommand = userCommandBR.readLine().split(" ");
+
+                if(userCommand[0].equalsIgnoreCase("e")) {
+                    break;
+                } else if(userCommand[0].equalsIgnoreCase("ls")) {
+                    System.out.println("Current storage units:\n");
+                } else if(userCommand[0].equalsIgnoreCase(COM_ADD_SU)) {
+                    if(userCommand.length > 1) {
+                        StorageUnit newStorageUnit = new StorageUnit(userCommand[1]);
+                        allStorageUnits.add(newStorageUnit);
+                        System.out.println("New storage unit '" + userCommand[1] + "' created\n");
+                    } else {
+                        System.out.println("Storage unit requires a name!\n");
+                    }
+                } else {
+                    System.out.println("Invalid command. Please try again or type help for a list of commands.\n");
+                }
+
             } catch(IOException badInput) {
-                System.out.println("Bad input. Please try again.");
+                System.out.println("Bad input. Please try again.\n");
             }
         }
 
--- src/foodstorage/StorageUnit.java
+++ src/foodstorage/StorageUnit.java
@@ -7,7 +7,7 @@ public class StorageUnit {
     private boolean atCapacity;
     FoodItem[] foodItems;
 
-    public static void lel() {
-        System.out.println("Again please");
+    public StorageUnit(String name) {
+        this.name = name;
     }
 }
